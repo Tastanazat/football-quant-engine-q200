a5ae70d998b074060fa7f8b8153309851b27cff2
Tastanazat <azat.tastan49@gmail.com>
2026-09-16 16:05:53 +0300

    Update __init__.py

--- q200_engine/__init__.py
+++ q200_engine/__init__.py
@@ -1,2 +1,72 @@
-"""Football Quant Engine Q200 - clean rebuild."""
-__version__ = "1.0.0"
+"""
+Q200 Engine
+
+Q200 V3.1
+"""
+
+from .schema import (
+    TeamStats,
+    ModelSnapshot,
+    OddsInput,
+    AnalysisResult,
+)
+
+from .model import (
+    calculate_lambdas,
+    build_model,
+)
+
+from .poisson_model import (
+    poisson_pmf,
+    poisson_score_matrix,
+    poisson_match_probabilities,
+)
+
+from .monte_carlo import (
+    simulate_match,
+)
+
+from .odds import (
+    implied_probabilities,
+    fair_odds,
+    expected_value,
+)
+
+from .selection import (
+    select,
+)
+
+from .kelly import (
+    quarter_kelly,
+)
+
+from .pipeline import (
+    Q200Pipeline,
+)
+
+
+__all__ = [
+    "TeamStats",
+    "ModelSnapshot",
+    "OddsInput",
+    "AnalysisResult",
+
+    "calculate_lambdas",
+    "build_model",
+
+    "poisson_pmf",
+    "poisson_score_matrix",
+    "poisson_match_probabilities",
+
+    "simulate_match",
+
+    "implied_probabilities",
+    "fair_odds",
+    "expected_value",
+
+    "select",
+
+    "quarter_kelly",
+
+    "Q200Pipeline",
+]
